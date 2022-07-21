2f0d3466781ede4930c91e843090114d0ef03c22
Marissa Tobin <marissatobin@192-168-1-100.tpgi.com.au>
2022-07-21 18:45:50 +0800

    update index file

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const markdown = require('./utils/generateMarkdown');
+
+// writeFile function using promises instead of a callback function
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -6,8 +13,15 @@ const questions = [];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((response) => {
+        let readmeContent = markdown.generateMarkdown(response)
+    console.log(response);
+    writeToFile('README.md', readmeContent)
+    }
+  )}
 
 // Function call to initialize app
 init();
